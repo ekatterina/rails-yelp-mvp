4d7b4febb330124ef72dd734d27de713a0472992
Ekaterina Tugarinova <kate.tugarinova@gmail.com>
2020-08-12 16:17:04 +0200

    railsyelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  name = Faker::Hipster.word
+  address = Faker::Address.street_address
+  phone_number = Faker::Number.number
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+
+  Restaurant.create(name: name, address: address, phone_number: phone_number, category: category)
+  puts "Restaurant #{name} created"
+end
